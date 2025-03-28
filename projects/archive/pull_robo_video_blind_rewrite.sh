b5aeefb3e46acdf8fb531ed50091dc3e265e2fb9
a1600012888 <tianyuanzhang@pku.edu.cn>
2025-03-28 13:58:42 -0400

    update

--- projects/archive/pull_robo_video_blind_rewrite.sh
+++ projects/archive/pull_robo_video_blind_rewrite.sh
@@ -1,9 +1,9 @@
 mkdir -p robo_videos_phase_2_blind_rewrite
 
 # for folder in droid ego4d epic_kitchens language_table pandas something
-for folder in bridge driod agibot
+for folder in bridge driod ego4d epic_kitchens
 do
     wget "https://huggingface.co/datasets/YunjinZhang/robo_videos_phase_2_blind_rewrite/resolve/main/${folder}/index.html" \
-       -O "robo_videos_phase_2/${folder}.html"
+       -O "robo_videos_phase_2_blind_rewrite/${folder}.html"
 done
 
