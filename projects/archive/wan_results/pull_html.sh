60f92c814b75f96c86b8d5d45ea8220c8f6be5fd
a1600012888 <tianyuanzhang@pku.edu.cn>
2025-03-31 09:55:24 -0400

    update

--- projects/archive/wan_results/pull_html.sh
+++ projects/archive/wan_results/pull_html.sh
@@ -4,14 +4,8 @@
 
 # Download each index.html file and rename them using the folder name
 
-wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/wandb_1.3b_t2v_shift_5.0/index.html" \
-     -O "wandb_1.3b_t2v_shift_5.0.html"
 
-wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/output_videos_wan_1.4B_baseline_ts3/index.html" \
-     -O "output_videos_wan_1.4B_baseline_ts3.html"
-
-wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/wandb_14b_timeshift_5.0/index.html" \
-     -O "wandb_14b_timeshift_5.0.html"
-
-wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/wandb_1.4b_ft_7kiters/index.html" \
-     -O "wandb_1.4b_ft_7kiters.html"
+wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/high_res_baseline_720p_ts5/index.html" \
+     -O "wanbaseline_high_res_720p_81f_ts5.html"
+wget "https://huggingface.co/datasets/YunjinZhang/video_results_tmp/resolve/main/long_baseline_161f/index.html" \
+     -O "wanbaseline_long_480p_161f_ts3.html"
